a6bde58d7db13aa196f15180dea39d0bde52d9bc
Alan <alanperdomo@hotmail.com>
2023-06-23 22:15:14 -0300

    .

--- Classes/ContaCorrente.java
+++ Classes/ContaCorrente.java
@@ -1,7 +1,7 @@
 package Classes;
 
 public class ContaCorrente extends Conta {
-    private static final double LIMITE_CHEQUE_ESPECIAL = 500.00;
+    private static final double CHEQUE_ESPECIAL = 500.00;
 
     public ContaCorrente(int numeroConta, String cpfPessoa, double saldo) {
         super(numeroConta, cpfPessoa, saldo);
@@ -14,7 +14,7 @@ public class ContaCorrente extends Conta {
 
     @Override
     public boolean sacar(double valor) {
-        double saldoTotal = getSaldo() + LIMITE_CHEQUE_ESPECIAL;
+        double saldoTotal = getSaldo() + CHEQUE_ESPECIAL;
         if (saldoTotal >= valor) {
             setSaldo(getSaldo() - valor);
             System.out.printf("Saque de %.2f realizado. Novo saldo: R$%.2f\n", valor, getSaldo());
